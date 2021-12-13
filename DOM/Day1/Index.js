9a121322dc4f02c5fe76caa701d95c62ffc21a68
mayankbhatt07141 <mayankbhatt07141@gmail.com>
2021-12-13 23:39:34 +0530

    Basics of Dom

---
+++ DOM/Day1/Index.js
@@ -0,0 +1,5 @@
+greet=()=>{
+document.getElementById("buttonGreet").innerText="Greeted";
+let name1=document.getElementById("InputName").value;
+
+}
---
+++ Digital_clock/Index.js
@@ -0,0 +1,6 @@
+let date=new Date();
+let hour=date.getHours();
+let min=date.getMinutes();
+let sec=date.getSeconds();
+
+document.getElementById("clock").innerText="mayank"
