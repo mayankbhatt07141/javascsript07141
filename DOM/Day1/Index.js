d610e5ee19498d1a98d813d4f8e593dea85a128a
mayankbhatt07141 <mayankbhatt07141@gmail.com>
2021-12-14 10:26:20 +0530

    Greeting completed

--- DOM/Day1/Index.js
+++ DOM/Day1/Index.js
@@ -1,5 +1,19 @@
-greet=()=>{
-document.getElementById("buttonGreet").innerText="Greeted";
-let name1=document.getElementById("InputName").value;
-
-}
+greetings = () => {
+  let date = new Date();
+  let hour = date.getHours();
+  if (hour >= 5 && hour < 12) {
+    return "Good Morning";
+  } else if ((hour >= 20 && hour <= 24) || (hour >= 0 && hour < 5)) {
+    return "Good Night";
+  } else if (hour >= 16 && hour < 20) {
+    return "good Evening";
+  } else if (hour > 12 && hour < 16) {
+    return "good Afternoon";
+  }
+};
+greet = () => {
+  document.getElementById("buttonGreet").innerText = "Greeted";
+  let name1 = document.getElementById("InputName").value;
+  document.getElementById("greetContainer").innerText =
+    "hy " + name1 + " " + greetings();
+};
