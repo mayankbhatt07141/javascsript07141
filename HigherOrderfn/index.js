ea137590296149cc7375d29119e63cacf6fa1a04
Mayank Bhatt <mayankbhatt07141@gmail.com>
2024-07-16 19:34:37 +0530

    Map pollyfill added

--- HigherOrderfn/index.js
+++ HigherOrderfn/index.js
@@ -1,24 +1,56 @@
-// let radius = [2, 5, 4, 7];
-// //higher order function
+// Function that takes function as an argement or returns function from it is called higher order function
 
-// function calculate(logic, arr) {
-//   //Higher order function
-//   output = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     output.push(logic(arr[i]));
-//   }
-//   console.log(x);
-//   return output;
-// }
-// function area(radius) {
-//   return Math.PI * radius * radius;
-// }
-// function parameter(radius) {
-//   return 2 * Math.PI * radius;
-// }
 
-// console.log(calculate(area, radius));
-// console.log(calculate(parameter, radius));
+let radius = [2, 5, 4, 7];
+
+
+function area(radius) {
+  return Math.PI * radius * radius;
+}
+
+function parameter(radius) {
+  return 2 * Math.PI * radius;
+}
+
+
+function calculate(logic, arr) {
+  //Higher order function
+  output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(logic(arr[i]));
+  }
+  console.log(x);
+  return output;
+}
+
+
+console.log(calculate(area, radius));
+console.log(calculate(parameter, radius));
+
+// here Calculate is higher order function as it takes function as an arguemnt
+
+function mapPolly(logic){ 
+        result = []
+        console.log(this)
+        for(let i=0;i<this.length; i++){
+          result.push(logic(this[i]))
+        }
+        
+        return result
+}
+
+let array = [1,2,3,4,5]
+
+function multiplybytwo(item){
+        return item*2;
+}
+
+Array.prototype.mapPolly = mapPolly;
+let grater2 = array.mapPolly(multiplybytwo)
+
+console.log(grater2)
+
+
 
 function x() {
   let a = 5;
@@ -30,7 +62,9 @@ function x() {
 let z = x();
 console.log(z());
 
-//Map
+
+
+//MAP and Reduce example:
 
 const users = [
   { firstname: "mayank", lastname: "bhatt", age: 26 },
@@ -38,9 +72,12 @@ const users = [
   { firstname: "mayank", lastname: "bhatt", age: 15 },
   { firstname: "mayank", lastname: "bhatt", age: 56 },
 ];
+
+
 let fullname = users.map((value) => {
   return value.firstname + value.lastname;
 });
+
 console.log(fullname);
 
 const output = users.reduce((acc, curr) => {
